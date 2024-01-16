e9c6a0fb73eb55fe59324daa93d6a89764df220a
leliso <lelisoagegneh491@gmail.com>
2024-01-16 16:35:36 +0300

    🐳 chore: middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { middleware } from './logger.middleware';
 
 @Module({
   imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+// export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(middleware).forRoutes('signIn');
+  }
+}
